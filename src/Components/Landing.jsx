124ee1823552fb41091f4397cc0da1c2c9888455
Adrish Mitra <adrishmitra710@gmail.com>
2023-10-21 18:07:19 +0530

    init

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './Hero'
+import Nav from './Nav'
+
+const Landing = () => {
+  return (
+    <div className='landing'>
+      <Nav/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/Components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="nav">
+        <ul>
+          <li><a href="#">// home</a></li>
+          <li><a href="#">// skills</a></li>
+          <li><a href="#">// work</a></li>
+          <li><a href="#">// education</a></li>
+          <li><a href="#">// contact</a></li>
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
