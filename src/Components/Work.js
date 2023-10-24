1779dcf2364801a378f29e409366f1787fc798a9
Adrish Mitra <adrishmitra710@gmail.com>
2023-10-24 22:44:31 +0530

    work page

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,19 +1,54 @@
 import React, { useState } from 'react'
+import works from '../Data/WorkData';
+import icon from '../Assets/skills-bg.jpg'
+import {BsArrowRight} from 'react-icons/bs'
 
 const Work = () => {
 
-  const [selected, setSelected] = useState(0);
+  const [workslist, setWorkList] = useState(works)
+  const [selected, setSelected] = useState('all');
+
+  const filterList = (currSel)=>{
+    setSelected(currSel)
+    const newList = works.filter(item =>{
+      if(currSel === 'all'){
+        return true
+      }
+      return item.category === currSel
+    })
+    setWorkList(newList)
+  }
 
   return (
     <div className='work snap-container' id='work'>
       <h1>My Work</h1>
       <div className="work-container">
         <h3>Filter by 
-          <span onClick={()=> setSelected(0)} className= {`${ selected === 0 ? 'selected' : ''} filter-options all`} id='filter-all'> All</span>/ 
-          <span onClick={()=> setSelected(1)} className={`${ selected === 1 ? 'selected' : ''} filter-options`} id='filter-web'> Web Development</span>/
-          <span onClick={()=> setSelected(2)} className={`${ selected === 2 ? 'selected' : ''} filter-options`} id='filter-python'> Python</span>
+          <span onClick={()=> filterList('all')} className={`${ selected === 'all' ? 'selected' : ''} filter-options all`} id='filter-all'> All</span>/ 
+          <span onClick={()=> filterList('web')} className={`${ selected === 'web' ? 'selected' : ''} filter-options`} id='filter-web'> Web Development</span>/
+          <span onClick={()=> filterList('python')} className={`${ selected === 'python' ? 'selected' : ''} filter-options`} id='filter-python'> Python</span>/
+          <span onClick={()=> filterList('java')} className={`${ selected === 'java' ? 'selected' : ''} filter-options`} id='filter-java'> Java</span>
         </h3>
 
+        <div className="work-card-container">
+          {
+            workslist.map(item =>{
+              return(
+                <div key={item.title} className="work-card">
+                  <div className="work-card-img-container">
+                    {item.img ? 
+                        <img src={item.img} alt="" /> :
+                        <img className='default-img' src={icon}></img>
+                    }
+                  </div>
+                  <h2>{item.title}</h2>
+                  <a href={item.link}>View Project <BsArrowRight style={{transform: 'translateY(3px)'}}/></a>
+                  <p>{item.category}</p>
+                </div>
+              )
+            })
+          }
+      </div>
 
       </div>
 
--- src/Data/WorkData.js
+++ src/Data/WorkData.js
@@ -1,4 +1,116 @@
+
+import BharatPe from '../Assets/WorkAssets/BharatPe.png' 
+import Blynk from '../Assets/WorkAssets/Blynk.png'
+import Cureazzy from '../Assets/WorkAssets/Cureazzy.png'
+import Netflix from '../Assets/WorkAssets/Netflix.png'
+import Omega from '../Assets/WorkAssets/Omega.png'
+import Over from '../Assets/WorkAssets/Over.png'
+import Todo from '../Assets/WorkAssets/Todo-React.png'
+import Video from '../Assets/WorkAssets/Video-Editing.png'
+
 const works = [
+    {
+        title: 'File Sorting and Organization App',
+        img: '',
+        category: 'python',
+        link: 'https://github.com/dark-byte/Sort-Files'
+    },
+
+    {
+        title : 'Cureazzy',
+        img : `${Cureazzy}`,
+        category : 'web',
+        link : 'https://profound-lily-baf58c.netlify.app/'
+    },
+    
+    {
+        title : 'Blynk Finance',
+        img : `${Blynk}`,
+        category : 'web',
+        link : 'https://stirring-pastelito-bf5200.netlify.app/'
+    },
+
+    {
+        title : 'Omega Opulence',
+        img : `${Omega}`,
+        category : 'web',
+        link : 'https://gleaming-vacherin-d5b7dd.netlify.app/'
+    },
+
+
+
+    {
+        title: 'Socket app with multithreading',
+        img: '',
+        category: 'java',
+        link: 'https://github.com/dark-byte/client-server'
+    },
+
+
+    {
+        title: 'Video Editing Portfolio',
+        img: `${Video}`,
+        category: 'web',
+        link: 'https://dark-byte.github.io/'
+    },
+
+
+    {
+        title : 'Over Website',
+        img : `${Over}`,
+        category : 'web',
+        link : 'https://dark-byte.github.io/Pixel-Bridge-Assignment/'
+    },
+
+
+    {
+        title: 'Netflix Landing Page',
+        img: `${Netflix}`,
+        category: 'web',
+        link: 'https://dark-byte.github.io/stige-04-netflix/'
+    },
+
+    {
+        title : 'Password Vault App',
+        img : '',
+        category : 'python',
+        link : 'https://github.com/dark-byte/Password-Vault-PY'
+    },
+
+    {
+        title : 'Amazon WebScraper',
+        img : '',
+        category : 'python',
+        link : 'https://github.com/dark-byte/WebScrapAssignment'
+    },
+
+    {
+        title: 'BharatPe Landing Page',
+        img: `${BharatPe}`,
+        category: 'web',
+        link: 'https://dark-byte.github.io/stige-02-frontend-copy/'
+    },
+
+    {
+        title: 'To-Do React App',
+        img: `${Todo}`,
+        category: 'web',
+        link: 'https://dark-byte.github.io/todo-react/'
+    },
+
+
+    {
+        title: 'Food Delivery System',
+        img: '',
+        category: 'python',
+        link: 'https://github.com/dark-byte/school_proj'
+    },
+    {
+        title: 'Automatic Zoom Meeting Attender',
+        img: '',
+        category: 'python',
+        link: 'https://github.com/dark-byte/A.Z.M.A'
+    }
 
 ]
 
