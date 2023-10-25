3a4e4d9017d69716b253fe579deb64b5ac0f4fa3
Adrish Mitra <adrishmitra710@gmail.com>
2023-10-25 23:43:55 +0530

    education accordion animation

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -19,7 +19,7 @@ const Education = () => {
         {
             eduList.map(item =>{
                 return(
-                    <>
+                    <div key = {item.id} style={{transition: 'all ease 0.5s'}}>
                         <div className={`${active === item.id? "edu-card edu-card-highlight" : 'edu-card'}`}id={item.id} onClick={()=>toggleActive(item.id)}>
                             <h3>{item.institution}</h3>
                             <div className="edu-card-right" >
@@ -31,13 +31,13 @@ const Education = () => {
                                 </h3>
                             </div>
                         </div>
-                        <div className="edu-info-animation" id={item.id}>
-                            <div className={`${active === item.id ? 'edu-card-info edu-view' : 'edu-card-info'}`}>
+                        <div className={`${active === item.id ? 'edu-card-info edu-view' : 'edu-card-info'}`}>
+                            <div style={{margin: '1rem 0', padding: '2rem'}}>
                                 <h3><MdLocationOn className='edu-location-icon'/> {item.location}</h3>
                                 <p>{item.info}</p>
                             </div>
                         </div>
-                    </>
+                    </div>
                 )
             })
         }
